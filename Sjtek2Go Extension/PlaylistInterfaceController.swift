1b97fc18a2ca1d8ea6ac5199df64295b42d38d70
Wouter Habets <wouterhabets@gmail.com>
2016-12-15 23:21:22 +0100

    Add watch app

---
+++ Sjtek2Go Extension/PlaylistInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  PlaylistInterfaceController.swift
+//  Sjtek
+//
+//  Created by Wouter Habets on 15/12/2016.
+//  Copyright © 2016 Sjtek. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class PlaylistInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
