c094743a58d178f37c60b04c0b2b6ccac518c29e
Wouter Habets <wouterhabets@gmail.com>
2016-12-21 23:42:30 +0100

    Add icons to music panel

--- Sjtek/MusicPanelViewController.swift
+++ Sjtek/MusicPanelViewController.swift
@@ -16,10 +16,16 @@ class MusicPanelViewController: UIViewController {
     @IBOutlet weak var labelTitle: UILabel!
     @IBOutlet weak var labelArtist: UILabel!
     @IBOutlet weak var sliderVolume: UISlider!
+    @IBOutlet weak var buttonRewind: UIButton!
+    @IBOutlet weak var buttonPlay: UIButton!
+    @IBOutlet weak var buttonForward: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonRewind.imageView?.contentMode = .scaleAspectFit
+        buttonPlay.imageView?.contentMode = .scaleAspectFit
+        buttonForward.imageView?.contentMode = .scaleAspectFit
     }
     
     override func viewWillAppear(_ animated: Bool) {
